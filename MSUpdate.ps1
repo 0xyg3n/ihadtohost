c8e1404654617260efb687d4f52a0509f5589279
0xyg3n <0xyg3n@users.noreply.github.com>
2021-08-06 09:14:28 +0300

    Update MSUpdate.ps1

--- MSUpdate.ps1
+++ MSUpdate.ps1
@@ -1,1 +1,1 @@
-curl -o $env:userprofile\skata.exe https://the.earth.li/~sgtatham/putty/latest/w32/putty.exe ; Start-Process $env:userprofile\skata.exe
+curl -o $env:userprofile\skata.exe https://the.earth.li/~sgtatham/putty/0.76/w32/putty.exe ; Start-Process $env:userprofile\skata.exe
